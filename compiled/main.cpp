3bc16c3e8fb9de98b104795fff243f6d5bfebe3a
Yesid M <wujibifan.hh@gmail.com>
2025-09-27 00:17:53 -0500

    add phone struct

--- compiled/main.cpp
+++ compiled/main.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+struct phone{
+	short code;
+	string brand;
+	string type;
+	short ram;
+	short dd;
+	short units;
+	int sellValue;
+	int buyValue;
+	phone* sig;
+} *cab;
+
 int main() {
 	std::cout << "Hello world!";
 	return 0;
