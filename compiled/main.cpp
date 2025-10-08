15fc2ddcfcb05017ade27bcc764e24308fd08156
Yesid M <wujibifan.hh@gmail.com>
2025-10-08 12:20:35 -0500

    implement phone sale process, stock validation and balance update

--- compiled/main.cpp
+++ compiled/main.cpp
@@ -146,6 +146,61 @@ void buyPhone(long &balance) {
     }
 }
 
+void sellPhone(long &balance) {
+	short cod = 0, quant = 0;
+
+    phone* i = head;
+
+    if (i == nullptr) {
+        std::cout << "- There are no available phones at this moment.\n";
+    } else {
+        std::cout << "Enter the code of the phone you want to sell: \n";
+        std::cin >> cod;
+
+        bool found = false;
+
+        while (i != nullptr) {
+            if (i->code == cod) {
+	
+            	if(i->stock <= 0){
+            		std::cout << "No stock available for model " << i->model << ".\n";
+            		return;
+				}
+
+            	found = true;
+
+                std::cout << "Enter quantity to sell: \n";
+                std::cin >> quant;
+
+                if (quant <= 0) {
+                    std::cout << "Please enter a quantity of at least one (1).\n";
+                	return;
+				}
+
+				if(i->stock < quant){
+					std::cout << "Insufficient stock. Available units: " << i->stock << "\n";
+                	return;
+				}
+
+                i->stock -= quant; 
+                balance += (quant * i->sellValue);
+                std::cout << " \n";
+                std::cout << "- " << quant << " phone(s) sold successfully!\n";
+                std::cout << "- Total sale value: $" << (quant * i->sellValue) << "\n";
+				std::cout << " \n";
+				std::cout << "- Transaction completed.\n";
+            	std::cout << " \n";
+            	return;
+			}
+            i = i->next;
+        }
+
+	    if (!found) {
+	        std::cout << "Phone code not found! Please try again.\n";
+	    }
+    }
+}
+
 void deletePhone(){
 	if(head == nullptr){
 		std::cout << "Inventory is empty.\n";
@@ -198,8 +253,9 @@ int main() {
 		std::cout << "1. Add phone.\n";
 		std::cout << "2. Delete phone.\n";
 		std::cout << "3. View inventory.\n";
-		std::cout << "4. Buy phone(s).\n";	
-		std::cout << "5. Exit.\n";	
+		std::cout << "4. Buy phone(s).\n";
+		std::cout << "5. Sell phone(s).\n";
+		std::cout << "6. Exit.\n";
 		
 		std::cin >> opt;
 		
@@ -217,13 +273,16 @@ int main() {
 				buyPhone(balance);
 				break;
 			case 5:
+				sellPhone(balance);
+				break;
+			case 6:
 				std::cout << "Closing app...\n";
 				break;
 			default:
 				std::cout << "Invalid option!\n";
 				break;
 		}
-	}while (opt != 5);
+	}while (opt != 6);
 	
 	return 0;
 }
